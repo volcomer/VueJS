03215643e03ab20d243f20199e98b5781e967181
Volcomer <volcomer@gmail.com>
2017-11-29 21:33:50 +0100

    fixd

--- src/components/views/WrapperNav.vue
+++ src/components/views/WrapperNav.vue
@@ -1,11 +1,9 @@
 <template>
        <nav class="navbar navbar-default navbar-fixed-top">
-
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -15,7 +13,7 @@
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse navbar-right" id="myNavbar">
                     <ul class="nav navbar-nav navbar-right">
 
                         <li class="active"><a class="fa fa-home" href="#home"> Home</a></li>
@@ -24,7 +22,6 @@
                         <li><a href="#team"> Team</a></li>
                         <li><a href="#blog"> Blog</a></li>
                         <li><a class="fa fa-phone" href="#contact"> Contact</a></li>
-
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
